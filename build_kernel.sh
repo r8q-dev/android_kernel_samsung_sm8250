c9a85e88719c1ad8ed81d8ce3e7d21fd35071e36
ata-kaner <ata_kaner@outlook.com>
2023-09-15 06:31:54 +0300

    defconfig: Rename: lineage->twrp

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,7 @@ ARCH=arm64
 # Toolchain paths
 PATH="/home/ata-kaner/los/prebuilts/clang/host/linux-x86/clang-r416183b/bin:/home/ata-kaner/los/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:/home/ata-kaner/los/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:${PATH}"
 
-make -j$(nproc --all) ARCH=$ARCH CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- vendor/lineage_r8q_defconfig
+make -j$(nproc --all) ARCH=$ARCH CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- vendor/twrp_r8q_defconfig
 
 make -j$(nproc --all) ARCH=$ARCH CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi-
 
